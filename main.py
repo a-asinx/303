c9c09225b11f525319db7be8f7631555823bf884
zero-60 <1300488582@qq.com>
2024-06-07 19:49:54 +0800

    仰卧起坐

---
+++ main.py
@@ -0,0 +1,89 @@
+import os
+from moviepy.editor import VideoFileClip
+
+
+# 检查该文件是否已保存过
+def is_exist(file_name,find_name):
+    results = []
+    with open(file_name, 'r') as file:
+        context = file.read()
+        # print(context)
+    word = list(context.split('\n'))
+    for k in range(len(word)):
+        results.append(word[k].split())
+    for i in range(len(results)):
+        if len(results[i]) != 0 and find_name == results[i][0]:
+            return True
+    file.close()
+    return False
+
+def get_file_duration(file_path):#获取视频时长
+    video = VideoFileClip(file_path)
+    duration = video.duration
+    return duration
+
+
+def get_file(s):
+    file_name='file.txt'
+    name=get_file_prefix(file_name)
+    # print(name)
+    #文件名 手肘最低点坐标值lx,ly 手肘最高点坐标值hx,hy
+    context=[]
+    results = []
+    with open(file_name, 'r') as file:
+        context = file.read()
+        # print(context)
+    word = list(context.split('\n'))
+    for k in range(len(word)):
+        results.append(word[k].split())
+    file.close()
+
+    for i in range(len(results)):
+        if len(results[i])!=0 and name==results[i][0]:
+            return results[i][1],results[i][2],results[i][3],results[i][4]
+    return 0,0,0,0
+#将打开过的文件信息存档
+def write_file_information(file_name,lx,ly,hx,hw):
+    print(file_name)
+    if is_exist('file.txt',file_name):
+        print('write_file_information:','The file has been recorded')
+        return True
+    with open('file.txt', 'a') as file:
+        file.write('\n')
+        file.write(file_name+' ')
+        file.write(str(lx)+' ')
+        file.write(str(ly)+' ')
+        file.write(str(hx)+' ')
+        file.write(str(hw))
+    file.close()
+
+# 存储视频结果
+def write_ywqz_results(file_name,count,uncount):
+    #文件名，合格个数 不合格个数 分数
+    #视频长度
+    duration=get_file_duration(file_name)
+    # print(count/duration)
+    #文件前缀
+    name=get_file_prefix(file_name)
+
+    #
+    if is_exist('ywqz.txt',name):
+        print('write_ywqz_results:','The file has been recorded')
+        return True
+
+    with open('ywqz.txt', 'a') as file:
+        file.write(name+' ')
+        file.write(str(count)+' ')
+        file.write(str(uncount)+' ')
+        file.write(str(int((count/duration)*100))+'\n') #分数=个数/视频时长
+
+#获取文件名称（前缀）
+def get_file_prefix(file_name):
+    filename_with_extension = os.path.basename(file_name)
+    name, _ = os.path.splitext(filename_with_extension)
+    return name
+
+
+#
+# get_file('file.txt')
+# write_ywqz_results('img/ywqz1.mp4',8,2)
---
+++ pose1.py
@@ -0,0 +1,105 @@
+import os
+
+import mediapipe as mp
+import cv2
+import warnings
+import main
+
+warnings.filterwarnings("ignore")
+
+mpPose=mp.solutions.pose
+pose=mpPose.Pose()
+
+file_name='img/ywqz5.mp4'
+
+
+def get_coordinate():
+    cap = cv2.VideoCapture(file_name)
+    lx, ly, hx, hy = 999, 999, 0, 0
+
+    filename_with_extension = os.path.basename(file_name)
+    name, _ = os.path.splitext(filename_with_extension)
+
+    lx, ly, hx, hy=main.get_file(name)
+    if lx==0:
+        lx, ly, hx, hy = 999, 999, 0, 0
+    else:
+        return int(lx), int(ly), int(hx), int(hy)
+
+    while True:
+        success, img = cap.read()
+        # print(results[0].pose_landmarks)
+        if not success:
+            print("视频结束")
+            break
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = pose.process(imgRGB)
+        print(results)
+        if results[0].pose_landmarks:
+            h, w, c = img.shape
+            cx = int(results[0].pose_landmarks.landmark[8].x * w)
+            cy = int(results[0].pose_landmarks.landmark[8].y * h)
+            cv2.circle(img, (cx, cy), 2, (0, 0, 255), -1)
+            # print('cx', 'cy', cx, cy)
+            if lx > cx:
+                lx = cx
+                ly = cy
+            elif hx < cx:
+                hx, hy = cx, cy
+    cap.release()
+    main.write_file_information(name,lx,ly,hx,hy)
+    return lx,ly,hx,hy
+
+def ywqz_detection():
+    ptime = 0
+    count = 0
+    count_not_standard = 0
+    cj = 0  # cj=0 到达最高点  cj=1 到达最低点
+    p = 50
+    lx, ly, hx, hy = get_coordinate()
+    cap=cv2.VideoCapture(file_name)
+    while True:
+        success, img = cap.read()
+        # print(results[0].pose_landmarks)
+        if not success:
+            print("视频结束")
+            break
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = pose.process(imgRGB)
+        if results[0].pose_landmarks:
+            h,w,c=img.shape
+            # print('h,w',h,w)
+            cx = int(results[0].pose_landmarks.landmark[8].x * w)
+            cy = int(results[0].pose_landmarks.landmark[8].y * h)
+            cv2.circle(img, (cx, cy), 3, (0, 0, 255), -1)
+            # print('cx', 'cy', cx, cy)
+            if cj==0 and lx-p<cx<lx+p and ly-p<cy<ly+p: #到达最低点
+                cj=1
+            elif cj==1 and hx-p<cx<hx+p and hy-p<cy<hy+p: #到达最高点
+                cj=0
+                count+=1
+            elif cj==1 and lx<cx<lx and ly<cy<ly: #未到达最高点但第二次到达最低点
+                count_not_standard+=1
+                cj=1
+            # elif cj==0 and hx<cx<hx and hy<cy<hy: #为到达最低点但第二次到达最高点
+            #     count_not_standard+=1
+            #     cj=1
+
+            cx = int(results[0].pose_landmarks.landmark[26].x * w)
+            cy = int(results[0].pose_landmarks.landmark[26].y * h)
+            cv2.circle(img, (cx, cy), 3, (0, 0, 255), -1)
+        # cTime=time.time()
+        # fps=1/(cTime-ptime)
+        # ptime=cTime
+        cv2.putText(img,str(count),(70,50),cv2.FONT_HERSHEY_PLAIN,3,
+            (255,0,0),3)
+        # print(count_not_standard)
+
+        cv2.imshow('img',img)
+        if cv2.waitKey(1) & 0xFF == ord('0'):
+            break
+
+    # 存储视频结果
+    main.write_ywqz_results(file_name, count, count_not_standard)
+
+ywqz_detection()
